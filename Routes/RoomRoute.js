7c09d57d5f5f5f14cab4716ab6bae86b336b1373
Gurashish Singh Gill <ggill_be19@thapar.edu>
2022-11-04 20:43:03 +0530

    backend v1

--- Routes/RoomRoute.js
+++ Routes/RoomRoute.js
@@ -1,9 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from "../Controllers/RoomController.js";
+import { verifyAdmin } from "../Utils/Verify.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Room");
-});
+// GET ROOM
+router.get("/get/:id", getRoom);
+
+// ADD ROOM
+router.post("/create/:hotelid", verifyAdmin, createRoom);
+
+// DELETE ROOM
+router.delete("/delete/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// UPDATE ROOM
+router.put("/update/:id", verifyAdmin, updateRoom);
+
+// GET ALL ROOMS
+router.get("/getall", getAllRooms);
 
 export default router;
